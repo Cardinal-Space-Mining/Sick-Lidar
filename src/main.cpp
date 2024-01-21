43434dd8f4c24a6201eeb38f3030e62c1a15e2ee
Sam Richter <hoodie888@outlook.com>
2024-01-20 22:11:21 -0600

    update cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <pcl/pcl_config.h>
+#include <sick_scan_xd_api/sick_scan_api.h>
 
 
 int main(int argc, char** argv) {
 
 	std::cout << "Functional entrypoint!?" << std::endl;
+	std::cout << "Linked PCL v" << PCL_VERSION_PRETTY << std::endl;
 
 }
