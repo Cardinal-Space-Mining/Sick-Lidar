f5cd909093bedea9f1fa7dd0043dbdeb325cf02b
Sam Richter <samrichter888@gmail.com>
2024-01-27 19:38:34 -0600

    working multiscan config

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,10 @@ int main(int argc, char** argv) {
 	std::cout << "Internally Linked PCL v" << ldrp::pclVer() << std::endl;
 
 	const ldrp::LidarApi lapi = ldrp::create();
-	ldrp::lidarInit(lapi, "../sick_multiscan.launch");
+	ldrp::lidarInit(lapi, "./sick_multiscan.launch");
 
 	using namespace std::chrono_literals;
-	std::this_thread::sleep_for(5s);
+	for(;;) std::this_thread::sleep_for(5s);
 
 	// SickScanApiHandle handle = SickScanApiCreate(0, nullptr);
 	// // SickScanApiInitByLaunchfile(handle, "");
--- src/shared/lidar_api.cpp
+++ src/shared/lidar_api.cpp
@@ -9,6 +9,11 @@
 
 namespace ldrp {
 
+	void customizedPointCloudMsgCb(SickScanApiHandle apiHandle, const SickScanPointCloudMsg* msg)
+	{
+	std::cout << "C++ PointCloudMsgCb: " << msg->width << " x " << msg->height << " pointcloud message received" << std::endl; // data processing to be done
+	}
+
 	/** Interfacing and Filtering Implementation */
 	struct LidarImpl {
 	public:
@@ -22,16 +27,21 @@ namespace ldrp {
 		}
 
 	public:
-		LidarImpl(int ss_argc = 0, char** ss_argv = nullptr) :
-			_handle(SickScanApiCreate(ss_argc, ss_argv))
+		LidarImpl(int ss_argc = 0, char** ss_argv = nullptr) //:
+			// _handle(SickScanApiCreate(ss_argc, ss_argv))
 		{
+			// this->_sick_status = SickScanApiLoadLibrary("libsick_scan_xd_shared_lib.so");
+			this->_handle = SickScanApiCreate(ss_argc, ss_argv);
+
 			std::cout << "Lidar Internal API Created." << std::endl;
 		}
 		~LidarImpl() {
 			std::cout << "Lidar Internal API Destroyed." << std::endl;
 
+			this->_sick_status = SickScanApiDeregisterCartesianPointCloudMsg(this->_handle, &customizedPointCloudMsgCb);
 			this->_sick_status = SickScanApiClose(this->_handle);
 			this->_sick_status = SickScanApiRelease(this->_handle);
+			// this->_sick_status = SickScanApiUnloadLibrary();
 			this->_handle = nullptr;
 		}
 
@@ -42,6 +52,11 @@ namespace ldrp {
 
 		void lidarInit(const char* file) {
 			this->_sick_status = SickScanApiInitByLaunchfile(this->_handle, file);
+			this->_sick_status = SickScanApiRegisterCartesianPointCloudMsg(this->_handle, &customizedPointCloudMsgCb);
+		}
+
+		static void sickCallback(SickScanApiHandle apiHandle, const SickScanPointCloudMsg* msg) {
+			std::cout << "Callback Recieved!?!?" << std::endl;
 		}
 
 	protected:
